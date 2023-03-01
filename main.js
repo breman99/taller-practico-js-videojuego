75e1c45dfdca0a528d360c791c98cf9b4c67ba6d
Breman <emabruno99@gmail.com>
2023-03-01 23:49:50 +0100

    primer Commit, canvas

---
+++ main.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+let canvasSize;
+let elementSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+	if(window.innerHeight > window.innerWidth) {
+		canvasSize = window.innerWidth * 0.8;
+	} else {
+		canvasSize = window.innerHeight * 0.8
+	}
+
+	canvas.setAttribute('width', canvasSize)
+	canvas.setAttribute('height', canvasSize)
+
+	elementSize = canvasSize / 10;
+
+	startGame();
+};
+
+function startGame() {
+	game.font = `${elementSize}px Verdana`;
+	game.textAlign = 'end';
+
+	for(let i = 1; i <= 10; i++){
+		game.fillText(emojis['X'], elementSize * i, elementSize);
+	};
+};
+
